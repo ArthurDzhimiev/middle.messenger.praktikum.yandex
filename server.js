1d9143f6514c30b3ba9c8c24401d6d85975cdd79
ArthurDzhimiev <71964936+ArthurDzhimiev@users.noreply.github.com>
2021-12-04 19:27:00 +0300

    update server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./dist/'));
+app.use(express.static(path.join(__dirname + '/dist')));
 
+app.use('/*', (req, res) => {
+  res.sendFile(__dirname + '/dist/index.html');
+})
 app.listen(PORT, () => {});
